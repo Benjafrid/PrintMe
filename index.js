a9f505fdce330dfb6c1283477efddc8e776a1dd8
Benjafrid <147622344+Benjafrid@users.noreply.github.com>
2024-11-05 11:24:07 -0300

    fixes index(conexion)

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ import cors from "cors"
 
 const app = express();
 app.use(express.json());
+
 const corsOptions = {
-  origin: ['*'],
+  origin: 'https://127.0.0.1:5500', // Especifica la URL exacta
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -16,14 +17,15 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Configuración de respuesta para solicitudes
+// Configuración de respuesta para solicitudes de opciones
 app.options('*', (_, res) => {
-  res.header("Access-Control-Allow-Origin",'https://127.0.0.1:5500');
+  res.header("Access-Control-Allow-Origin", 'https://127.0.0.1:5500');
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
   res.sendStatus(200);
 });
+
 
 app.get("/", async(_,res) =>{
  res.send("API WORKING")
